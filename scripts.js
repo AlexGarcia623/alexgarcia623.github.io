517869154d52f4bb2998d08a1d804e903acc45b1
AlexGarcia623 <alexmg2@illinois.edu>
2021-04-30 12:21:59 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+function home() {
+  window.open("https://alexgarcia623.github.io/website","_self");
+}
 
+function linkedIn() {
+  window.open("https://www.linkedin.com/in/alex-garcia-051300210/","_self")
+}
+
+function CV() {
+  var file = "Resume.pdf";
+  window.open(file,"_self");
+}
+  
+function secondPage() {
+  window.open("https://alexgarcia623.github.io/website/contactMe.html","_self");
+}
